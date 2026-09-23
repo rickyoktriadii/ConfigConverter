27c737b69ee97f4fa2a27d0816771eda9f78dd3f
rickyoktriadii <rickyoktriadii@gmail.com>
2026-09-24 02:45:55 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,37 +15,77 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables { useSupportLibrary = true }
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
-    buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
+    val composeBom =
+        platform("androidx.compose:compose-bom:2024.12.01")
+
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-compose:2.8.7"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7"
+    )
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
 
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+
+    implementation("androidx.compose.material3:material3")
+    implementation(
+        "androidx.compose.material:material-icons-extended"
+    )
+
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.1"
+    )
+
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0"
+    )
+
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3"
+    )
+
     implementation("org.yaml:snakeyaml:2.3")
+
     implementation("com.google.zxing:core:3.5.3")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+
+    testImplementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0"
+    )
 }
